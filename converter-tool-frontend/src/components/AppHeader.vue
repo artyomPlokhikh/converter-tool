537dcb02f7b55b500d14905e60f95891fc3a9fa3
artyom <plokhikhartyom10@gmail.com>
2025-04-20 03:00:02 +0200

    capitalize item names in AppHeader component

--- converter-tool-frontend/src/components/AppHeader.vue
+++ converter-tool-frontend/src/components/AppHeader.vue
@@ -17,7 +17,7 @@
 							:class="item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'"
 							:aria-current="item.current ? 'page' : undefined"
 						>
-							{{ item.name }}
+							{{ capitalize(item.name) }}
 						</router-link>
 					</div>
 				</div>
@@ -27,7 +27,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { capitalize, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter()
